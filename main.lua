45189bf2f5f7d0bf11270d136f7f5b12bfb110f7
DreadfulCrazy <lucasospina97@gmail.com>
2023-01-23 21:30:54 -0500

    PlayState added to state Machine map

--- main.lua
+++ main.lua
@@ -107,7 +107,8 @@ function love.load()
     -- 5. 'victory' (the current level is over, with a victory jingle)
     -- 6. 'game-over' (the player has lost; display score and allow restart)
     gStateMachine = StateMachine {
-        ['start'] = function() return StartState() end
+        ['start'] = function() return StartState() end,
+        ['play'] = function () return PlayState() end
     }
     gStateMachine:change('start')
 
